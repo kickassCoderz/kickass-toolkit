a81685428b0ff08d481544f7fe89efa1e0e65e9a
capJavert <ante@kickass.website>
2022-05-28 15:09:34 +0200

    feat: add initialValue prop

--- libs/hooks/src/lib/useStorageState/useStorageState.ts
+++ libs/hooks/src/lib/useStorageState/useStorageState.ts
@@ -1,16 +1,24 @@
-import { Dispatch, SetStateAction, useEffect } from 'react'
+import { Dispatch, SetStateAction, useEffect, useRef } from 'react'
 import { useState } from 'react'
 
 import { useEvent } from '../useEvent'
 import { useGlobalObject } from '../useGlobalObject'
 
-const useStorageState = (key: string, storage: Storage): [string | null, Dispatch<SetStateAction<string | null>>] => {
+const useStorageState = (
+    key: string,
+    storage: Storage,
+    initialState: string | null | (() => string | null) = null
+): [string | null, Dispatch<SetStateAction<string | null>>] => {
+    const mountedRef = useRef(false)
+
     const [value, setValue] = useState(() => {
+        const initialValue = typeof initialState === 'function' ? initialState() : initialState
+
         if (typeof storage === 'undefined') {
-            return null
+            return initialValue
         }
 
-        return storage.getItem(key)
+        return storage.getItem(key) || initialValue
     })
 
     const setStorageValue = useEvent((valueOrSetter: SetStateAction<string | null>) => {
@@ -26,14 +34,22 @@ const useStorageState = (key: string, storage: Storage): [string | null, Dispatc
     })
 
     useEffect(() => {
+        if (!mountedRef.current) {
+            mountedRef.current = true
+
+            return
+        }
+
         setValue(storage.getItem(key))
     }, [key, storage])
 
     return [value, setStorageValue]
 }
 
-const useLocalStorageState = (key: string) => useStorageState(key, useGlobalObject().localStorage)
+const useLocalStorageState = (key: string, initialState: string | null | (() => string | null) = null) =>
+    useStorageState(key, useGlobalObject().localStorage, initialState)
 
-const useSessionStorageState = (key: string) => useStorageState(key, useGlobalObject().sessionStorage)
+const useSessionStorageState = (key: string, initialState: string | null | (() => string | null) = null) =>
+    useStorageState(key, useGlobalObject().sessionStorage, initialState)
 
 export { useLocalStorageState, useSessionStorageState, useStorageState }
