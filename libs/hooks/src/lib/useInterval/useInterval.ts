011fe4b19382b21390207ed1e5cba2a3efa207ad
capJavert <ante@kickass.website>
2022-06-02 16:13:34 +0200

    feat: initial meat

--- libs/hooks/src/lib/useInterval/useInterval.ts
+++ libs/hooks/src/lib/useInterval/useInterval.ts
@@ -3,7 +3,16 @@ import { useEffect } from 'react'
 import { useEvent } from '../useEvent'
 
 const useInterval = (callback: () => void, delay: number) => {
-    // TODO
+    const onInterval = useEvent(callback)
+
+    useEffect(() => {
+        const interval = setInterval(onInterval, delay)
+
+        return () => {
+            clearInterval(interval)
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [delay])
 }
 
 export { useInterval }
