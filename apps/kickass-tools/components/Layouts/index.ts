aecf7a03c4942e3979f23bef1b8c50a975c42702
Saša Fazlić <45801671+Fazla-GroM@users.noreply.github.com>
2022-07-02 14:20:33 +0200

    feat(docs): init docosaurus (#48)

--- apps/kickass-tools/components/Layouts/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './DocsLayout'
-export * from './LandingLayout'
--- apps/kickass-tools/next-seo.config.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultSeoProps } from 'next-seo'
-
-const defaultSeoConfig: DefaultSeoProps = {
-    defaultTitle: 'Kickass Toolkit',
-    titleTemplate: 'Kickass Toolkit | %s',
-    description: 'Kickass Toolkit is a collection of tools for web development.'
-}
-
-export default defaultSeoConfig
