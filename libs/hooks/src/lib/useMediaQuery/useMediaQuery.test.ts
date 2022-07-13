20d3e20dba565d2d6e17d555a1239a2a3ff09794
capJavert <ante@kickass.website>
2022-07-13 17:50:24 +0200

    feat: initial meat

--- libs/hooks/src/lib/useMediaQuery/useMediaQuery.test.ts
+++ libs/hooks/src/lib/useMediaQuery/useMediaQuery.test.ts
@@ -3,13 +3,43 @@ import { renderHook } from '@testing-library/react'
 import { useMediaQuery } from './useMediaQuery'
 
 describe('useMediaQuery', () => {
+    const initialMM = global.matchMedia
+
+    beforeAll(() => {
+        global.matchMedia = jest.fn(query => {
+            return {
+                matches: query.includes('max-width: 600px'),
+                media: query,
+                onchange: null,
+                addListener: jest.fn(),
+                removeListener: jest.fn(),
+                addEventListener: jest.fn(),
+                removeEventListener: jest.fn(),
+                dispatchEvent: jest.fn()
+            }
+        })
+    })
+
+    afterAll(() => {
+        global.matchMedia = initialMM
+    })
+
     it('should be defined', () => {
         expect(useMediaQuery).toBeDefined()
     })
 
     it('should render', () => {
-        const { result } = renderHook(() => useMediaQuery())
+        const { result } = renderHook(() => useMediaQuery('(max-width: 600px)'))
 
         expect(typeof result.current).toBe('boolean')
+    })
+
+    it('should update value when document changes', () => {
+        const { result } = renderHook(() => useMediaQuery('(max-width: 600px)'))
+
+        expect(result.current).toBe(true)
+
+        // TODO feat/use-media-query
+        // expect(result.current).toBe(false)
     })
 })
--- libs/hooks/src/lib/useMediaQuery/useMediaQuery.ts
+++ libs/hooks/src/lib/useMediaQuery/useMediaQuery.ts
@@ -1,5 +1,38 @@
-const useMediaQuery = () => {
-    return true
+import { useEffect, useState } from 'react'
+
+import { useIsBrowser } from '../useIsBrowser'
+
+const useMediaQuery = (query: string, fallbackValue?: boolean) => {
+    const isBrowser = useIsBrowser()
+    const isSSRMode = typeof fallbackValue !== 'undefined'
+
+    const [matches, setMatches] = useState(() => {
+        if (!isBrowser && isSSRMode) {
+            return fallbackValue
+        }
+
+        return matchMedia(query).matches
+    })
+
+    useEffect(() => {
+        const mediaQueryList = matchMedia(query)
+
+        if (isSSRMode) {
+            setMatches(mediaQueryList.matches)
+        }
+
+        const handleChange = (event: MediaQueryListEvent) => {
+            setMatches(event.matches)
+        }
+
+        mediaQueryList.addEventListener('change', handleChange)
+
+        return () => {
+            mediaQueryList.removeEventListener('change', handleChange)
+        }
+    }, [query, isSSRMode])
+
+    return matches
 }
 
 export { useMediaQuery }
