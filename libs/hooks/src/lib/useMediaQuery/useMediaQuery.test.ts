75cd7ed2ac91bf869861d33d06ad7ca9876d16df
capJavert <ante@kickass.website>
2022-07-18 13:05:03 +0200

    test: change listener cleanup

--- libs/hooks/src/lib/useMediaQuery/useMediaQuery.test.ts
+++ libs/hooks/src/lib/useMediaQuery/useMediaQuery.test.ts
@@ -4,6 +4,8 @@ import { useMediaQuery } from './useMediaQuery'
 
 describe('useMediaQuery', () => {
     const initialMM = global.matchMedia
+    const onChangeSpy = jest.fn()
+    const offChangeSpy = jest.fn()
 
     beforeAll(() => {
         global.matchMedia = jest.fn(query => {
@@ -13,11 +15,16 @@ describe('useMediaQuery', () => {
                 onchange: null,
                 addListener: jest.fn(),
                 removeListener: jest.fn(),
-                addEventListener: jest.fn(),
-                removeEventListener: jest.fn(),
+                addEventListener: onChangeSpy,
+                removeEventListener: offChangeSpy,
                 dispatchEvent: jest.fn()
             }
         })
+    })
+
+    beforeEach(() => {
+        onChangeSpy.mockClear()
+        offChangeSpy.mockClear()
     })
 
     afterAll(() => {
@@ -31,6 +38,7 @@ describe('useMediaQuery', () => {
     it('should render', () => {
         const { result } = renderHook(() => useMediaQuery('(max-width: 600px)'))
 
+        expect(onChangeSpy).toHaveBeenCalledTimes(1)
         expect(typeof result.current.matches).toBe('boolean')
     })
 
@@ -48,5 +56,30 @@ describe('useMediaQuery', () => {
 
         // TODO feat/use-media-query
         // expect(result.current.matches).toBe(false)
+    })
+
+    it('should cleanup change listener on component unmount', () => {
+        const { unmount } = renderHook(() => useMediaQuery('(max-width: 600px)'))
+
+        expect(onChangeSpy).toHaveBeenCalledTimes(1)
+
+        unmount()
+
+        expect(offChangeSpy).toHaveBeenCalledTimes(1)
+    })
+
+    it('should cleanup change listener on query change', () => {
+        const { rerender } = renderHook(({ query }) => useMediaQuery(query), {
+            initialProps: {
+                query: '(max-width: 600px)'
+            }
+        })
+
+        expect(onChangeSpy).toHaveBeenCalledTimes(1)
+
+        rerender({ query: '(max-width: 800px)' })
+
+        expect(onChangeSpy).toHaveBeenCalledTimes(2)
+        expect(offChangeSpy).toHaveBeenCalledTimes(1)
     })
 })
