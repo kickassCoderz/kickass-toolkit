084f57ecbe1d7d59d998bec2066255d14c24f909
Sasa Fazlic <fazlic.sasa@gmail.com>
2022-10-26 10:25:25 +0200

    feat(toolkit-docs): make docs work again

--- apps/kickass-docs/sidebars.js
+++ apps/kickass-docs/sidebars.js
@@ -1,14 +1,3 @@
-/**
- * Creating a sidebar enables you to:
- - create an ordered group of docs
- - render a sidebar for each doc of that group
- - provide next/previous navigation
-
- The sidebars can be generated from the filesystem, or explicitly defined here.
-
- Create as many sidebars as you want.
- */
-
 // @ts-check
 
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
@@ -30,4 +19,5 @@ const sidebars = {
    */
 }
 
+// eslint-disable-next-line no-undef
 module.exports = sidebars
