89c114a3763ee740d858758753b29ee89f44bd86
capJavert <ante@kickass.website>
2022-11-24 11:47:32 +0100

    fix: lint issues

--- packages/rest-data-service/jest.config.js
+++ packages/rest-data-service/jest.config.js
@@ -3,10 +3,9 @@ module.exports = {
     displayName: 'rest-data-service',
     testEnvironment: 'jsdom',
     transform: {
-        '^.+\\.(t|j)sx?$': ['@swc/jest', { jsc: { transform: { react: { runtime: 'automatic' } } } }]
+        '^.+\\.(t|j)s$': ['@swc/jest']
     },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    // coverageDirectory: '../../coverage/packages/hooks',
+    moduleFileExtensions: ['ts', 'js', 'json', 'node'],
     coveragePathIgnorePatterns: ['index.ts'],
     setupFilesAfterEnv: ['./jest.setup.js']
 }
