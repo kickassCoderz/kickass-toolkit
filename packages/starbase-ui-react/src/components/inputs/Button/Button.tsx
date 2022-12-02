57eaf623a987f40ac61a628293a697aa7b7895b1
Sasa Fazlic <fazlic.sasa@gmail.com>
2022-12-02 10:37:28 +0100

    feat(starbase-ui): created basic button

--- packages/starbase-ui-react/src/components/inputs/Button/Button.tsx
+++ packages/starbase-ui-react/src/components/inputs/Button/Button.tsx
@@ -1,7 +1,20 @@
+import { forwardRef } from 'react'
+
+import type { TButtonBlueprint } from './button.css'
 import { buttonBlueprint } from './button.css'
 
-const Button = ({ children }) => {
-    return <button className={buttonBlueprint}>{children}</button>
-}
+type TButtonProps = Omit<React.ComponentPropsWithRef<'button'>, 'color'> & TButtonBlueprint
+
+const Button = forwardRef<HTMLButtonElement, TButtonProps>(
+    ({ children, shape = 'rounded', size = 'md', variant = 'solid', color = 'primary', ...rest }, ref) => {
+        return (
+            <button ref={ref} className={buttonBlueprint({ shape, size, variant, color })} {...rest}>
+                {children}
+            </button>
+        )
+    }
+)
+
+Button.displayName = 'Button'
 
 export { Button }
