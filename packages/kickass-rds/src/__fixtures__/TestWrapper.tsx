901ad526161d7205185b58ff7c92302ee1425883
capJavert <ante@kickass.website>
2023-04-02 22:03:22 +0200

    feat: add support for queryClient injection during tests

--- packages/kickass-rds/src/__fixtures__/TestWrapper.tsx
+++ packages/kickass-rds/src/__fixtures__/TestWrapper.tsx
@@ -4,39 +4,17 @@ import { useState } from 'react'
 import { DataServiceProvider } from '../providers'
 import { RestDataService } from '../services'
 import { MOCK_API_BASE_URL } from './consts'
+import { createTestQueryClient } from './createTestQueryClient'
 
-const TestWrapper = ({ children }: { children?: React.ReactNode }) => {
-    const [dataService] = useState(new RestDataService(MOCK_API_BASE_URL))
-    const [queryClient] = useState(
-        new QueryClient({
-            defaultOptions: {
-                queries: {
-                    retry: false
-                }
-            },
-            logger: {
-                log: console.log,
-                warn: console.warn,
-                // eslint-disable-next-line @typescript-eslint/no-empty-function
-                error: () => {}
-            }
-        })
-    )
-    // const dataService = new RestDataService(MOCK_API_BASE_URL)
-
-    // const queryClient = new QueryClient({
-    //     defaultOptions: {
-    //         queries: {
-    //             retry: false
-    //         }
-    //     },
-    //     logger: {
-    //         log: console.log,
-    //         warn: console.warn,
-    //         // eslint-disable-next-line @typescript-eslint/no-empty-function
-    //         error: () => {}
-    //     }
-    // })
+const TestWrapper = ({
+    children,
+    queryClient: injectedQueryClient
+}: {
+    children?: React.ReactNode
+    queryClient?: QueryClient
+}) => {
+    const [dataService] = useState(() => new RestDataService(MOCK_API_BASE_URL))
+    const [queryClient] = useState(() => injectedQueryClient || createTestQueryClient())
 
     return (
         <DataServiceProvider dataService={dataService} queryClient={queryClient}>
