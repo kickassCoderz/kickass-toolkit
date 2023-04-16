872155205f3ec551daa9d6bde833bda57b667c4f
Saša Fazlić <45801671+Fazla-GroM@users.noreply.github.com>
2023-04-16 15:06:05 +0200

    feat(kickass-js): created pipe (#95)
    * feat(kickass-js): created pipe and toPipe functions
    
    * feat(kickass-js): added some default types

---
+++ packages/kickass-js/src/curries/pipe/pipe.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { AnyUnaryFunction } from '../../types'
+
+export type PipeInitalValue<T extends AnyUnaryFunction[]> = Parameters<T[0]>[0]
+
+export type PipeReturnType<T extends AnyUnaryFunction[]> = T extends [...any[], (argument: any) => infer R] ? R : never
+
+export type PipeArguments<T extends AnyUnaryFunction[], Accumulator extends AnyUnaryFunction[] = []> = T extends [
+    (argument: infer A) => infer B
+]
+    ? [...Accumulator, (argument: A) => B]
+    : T extends [(argument: infer A) => any, ...infer Tail]
+    ? Tail extends [(argument: infer B) => any, ...any[]]
+        ? PipeArguments<Tail, [...Accumulator, (argument: A) => B]>
+        : Accumulator
+    : Accumulator
+
+export type PipeFunctions<T extends AnyUnaryFunction[]> = PipeArguments<T> extends T ? T : PipeArguments<T>
+
+/**
+ * Executes a list of `unary` functions in non mathematical order from left to right.
+ * @param initialValue - The initial value to pass to the first function in the pipe. It should be of same type as the argument of the first function in the pipe.
+ * @param pipeFunctions - A list of `unary` functions to execute in non mathematical order from left to right.
+ * @returns The result of the last function in the pipe.
+ */
+function pipe<T extends AnyUnaryFunction[]>(
+    initialValue: PipeInitalValue<T>,
+    ...pipeFunctions: PipeFunctions<T>
+): PipeReturnType<T> {
+    let result = initialValue
+
+    for (const functionToExecute of pipeFunctions) {
+        result = functionToExecute(result) as PipeFunctions<T>[number]
+    }
+
+    return result as PipeReturnType<T>
+}
+
+export { pipe }
---
+++ packages/kickass-js/src/types/index.ts
@@ -0,0 +1,2 @@
+// @NOTE: Types here should be considered to be moved to the @kickass-types package in the future
+export * from './function'
